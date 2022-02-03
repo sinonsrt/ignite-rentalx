5b9d7b169d905c644d40d698a827b310094d8c4c
Lucas Marchiori <sinonsrt@hotmail.com>
2022-02-02 21:47:16 -0300

    create categories route and add uuid

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
+
+app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.get("/", (request, response) => response.json("Hello Browwwwwwwwwwww 🚀"));
 
