6513da9fc4ffc3c10572b8e7d233a3afaf98bb85
Lucas Marchiori <sinonsrt@hotmail.com>
2022-02-03 22:54:07 -0300

    create categories repository and create method

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,24 +1,17 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (request, response) => {
-  const { name, description } = request.body;
+  const { description, name } = request.body;
 
-  const category = new Category();
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  categoriesRepository.create({ description, name });
 
-  categories.push(category);
-
-  return response.status(201).json({ categories });
+  return response.status(201).send();
 });
 
 export { categoriesRoutes };
