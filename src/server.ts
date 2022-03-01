8b45ab87133f937396677c07af20fa67204f0877
Lucas Marchiori <sinonsrt@hotmail.com>
2022-03-01 13:42:39 -0300

    add swagger and start docs

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.get("/", (request, response) => response.json("Hello Browwwwwwwwwwww 🚀"));
 
