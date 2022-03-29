5a3d1e16a93d9be20f19675e4f7718de7268ca11
Lucas Marchiori <sinonsrt@hotmail.com>
2022-03-29 20:09:29 -0300

    Create user migration and entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuidv4 } from "uuid";
+
+@Entity("users")
+class User {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  driver_license: string;
+
+  @Column({ default: false })
+  admin: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidv4();
+    }
+  }
+}
+
+export { User };
