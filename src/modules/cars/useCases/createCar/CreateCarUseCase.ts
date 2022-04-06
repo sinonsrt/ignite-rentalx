4fd702cf90910f0edcc3490bcbd6fbe679a52138
Lucas Marchiori <sinonsrt@hotmail.com>
2022-04-05 22:22:17 -0300

    add husky and eslint

--- src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,5 +1,3 @@
-import { inject, injectable } from "tsyringe";
-
 import { ICreateCarDTO } from "@modules/cars/dtos/ICreateCarDTO";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
 import { ICarsRepository } from "@modules/cars/repositories/ICarsRepository";
