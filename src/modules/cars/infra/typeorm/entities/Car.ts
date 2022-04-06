0ef3e30e9f79997194d54fcd04f8ee7e3b9588c6
Lucas Marchiori <sinonsrt@hotmail.com>
2022-04-05 21:39:23 -0300

    create cars useCase and test tdd

---
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -0,0 +1,12 @@
+class Car {
+  id: string;
+  name: string;
+  description: string;
+  daily_rate: number;
+  license_plate: string;
+  fine_amount: number;
+  brand: string;
+  category_id: string;
+}
+
+export { Car };
