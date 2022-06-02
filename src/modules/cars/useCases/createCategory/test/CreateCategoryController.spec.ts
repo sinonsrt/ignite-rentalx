f5ccebdc4a16554d8808b88d699723b3ab3b52fe
Lucas Marchiori <sinonsrt@hotmail.com>
2022-06-01 22:21:04 -0300

    create categories e2e test

--- src/modules/cars/useCases/createCategory/test/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/test/CreateCategoryController.spec.ts
@@ -1,14 +1,52 @@
-jest.useFakeTimers();
-
 import request from "supertest";
 import { app } from "@shared/infra/http/app";
+import { hash } from "bcryptjs";
+import { v4 as uuidv4 } from "uuid";
+import createConnection from "@shared/infra/typeorm";
+import { Connection } from "typeorm";
+
+let connection: Connection;
 
 describe("Create Category Controller", () => {
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuidv4();
+    const password = await hash("admin", 8);
+
+    await connection.query(
+      `INSERT INTO 
+        users
+        (id, name, password, email, admin, created_at, driver_license)
+        values('${id}', 'admin', '${password}', 'admin@rentalx.com.br', true, 'now()', 'XXXXXXX')
+      `
+    );
+  });
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it("should be able to create a new category ", async () => {
-    // const response = await request(app).post("/categories").send({
-    //   name: "Category Supertest",
-    //   description: "Category Supertest",
-    // });
-    // expect(response.status).toBe(201);
+    const responseToken = await request(app).post("/sessions").send({
+      email: "admin@rentalx.com.br",
+      password: "admin",
+    });
+
+    const { token } = responseToken.body;
+
+    const response = await request(app)
+      .post("/categories")
+      .send({
+        name: "Category Supertest",
+        description: "Category Supertest",
+      })
+      .set({
+        Authorization: `Bearer ${token}`,
+      });
+
+    expect(response.status).toBe(201);
   });
 });
--- src/shared/infra/typeorm/seed/admin.ts
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import createConnection from "../index";
 
 const create = async () => {
-  const connection = await createConnection("localhost");
+  const connection = await createConnection();
 
   const id = uuidv4();
   const password = await hash("admin", 8);
